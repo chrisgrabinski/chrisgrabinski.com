fbe7086e9a343a763724feead18cfee765578da9
Chris Grabiński <hello@chrisgrabinski.com>
2020-12-03 17:56:09 +0000

    chore: added emotion and tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import { GlobalStyles } from "twin.macro";
+import { CacheProvider } from "@emotion/react";
+import { cache } from "@emotion/css";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={cache}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
