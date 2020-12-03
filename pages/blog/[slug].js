2424249000d6ea8ed3d46959038401ad5bdffb4f
Chris Grabiński <hello@chrisgrabinski.com>
2020-12-03 19:06:28 +0000

    chore: create routes for blog and blog posts

---
+++ pages/blog/[slug].js
@@ -0,0 +1,37 @@
+import { getPaths, getEntry } from "../../lib/contentful";
+
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function BlogPostPage({ data }) {
+  return (
+    <div>
+      <h1>{data.title}</h1>
+    </div>
+  );
+}
+
+BlogPostPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+const CONTENT_MODEL = "blogPost";
+
+export async function getStaticProps({ params: { slug } }) {
+  const data = await getEntry(CONTENT_MODEL, slug, undefined, "title");
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = await getPaths(CONTENT_MODEL);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,34 @@
+import { getEntries } from "../../lib/contentful";
+
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+
+export default function BlogIndexPage({ posts }) {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.sys.id}>
+            <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+BlogIndexPage.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object),
+};
+
+export async function getStaticProps() {
+  const posts = await getEntries("blogPost", "title slug");
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
