46856fd4e61baa32fe89a7973e2d8e42427e7888
Chris Grabiński <c.grabinski@gmail.com>
2025-12-14 22:44:57 +0000

    add basic text to autra homepage

--- src/app/aura/page.tsx
+++ src/app/aura/page.tsx
@@ -1,9 +1,13 @@
-import { AuraLogo } from "@/app/aura/aura-logo";
-
 export default function RootPage() {
   return (
-    <div className="grid size-full place-items-center">
-      <AuraLogo className="text-6xl" />
+    <div className="grid gap-1.5">
+      <h1 className="font-semibold text-4xl">Welcome to Aura</h1>
+      <p className="neutral-500 text-balance text-lg text-neutral-600">
+        This is a work in progress collection of components and patterns for
+        building modern web applications. It's a playground for me to experiment
+        with new ideas and concepts while creating a comprehensive design
+        system.
+      </p>
     </div>
   );
 }
