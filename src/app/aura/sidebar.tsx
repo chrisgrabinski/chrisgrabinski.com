d271c92982b79dbdac33e180018d275d5f743667
Chris Grabiński <c.grabinski@gmail.com>
2025-12-16 14:33:10 +0000

    improve sidebar footer styles

--- src/app/aura/sidebar.tsx
+++ src/app/aura/sidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import avatar from "@/app/(homepage)/images/chris-grabinski.jpg";
 import { AuraLogo } from "@/app/aura/aura-logo";
 import { Navigation } from "@/app/aura/navigation";
+import { AgentRing } from "@/components/agent-ring";
 import { Avatar } from "@/components/avatar";
 import { Card } from "@/components/card";
 import { ScrollArea } from "@/components/scroll-area";
@@ -19,12 +20,19 @@ export const Sidebar = () => {
         <Navigation />
       </ScrollArea>
       <footer className="p-4">
-        <Link href="/">
-          <Card className="relative flex items-center gap-3 overflow-visible bg-neutral-900 p-3 text-neutral-50">
-            <Avatar fallback="Chris Grabinski" size={3} src={avatar.src} />
+        <Link className="group" href="/">
+          <Card className="flex items-center gap-3 overflow-visible p-3">
+            <div className="relative flex">
+              <div className="pointer-events-none absolute inset-0 z-10 rounded-full opacity-0 transition duration-300 group-hover:opacity-100">
+                <AgentRing animated glow />
+              </div>
+              <Avatar fallback="Chris Grabinski" size={3} src={avatar.src} />
+            </div>
             <div>
-              <p className="font-medium text-sm">Created by</p>
-              <p className="text-sm">Chris Grabiński</p>
+              <p className="text-neutral-600 text-sm">Created by</p>
+              <p className="duraiton-300 font-medium text-sm transition group-hover:text-pink-500">
+                Chris Grabiński
+              </p>
             </div>
           </Card>
         </Link>
