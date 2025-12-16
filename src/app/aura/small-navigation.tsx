1146b0781f6365ce201cf3ecd00046665a40e732
Chris Grabiński <c.grabinski@gmail.com>
2025-12-16 16:02:58 +0000

    adjust small navigation colors

--- src/app/aura/small-navigation.tsx
+++ src/app/aura/small-navigation.tsx
@@ -15,11 +15,11 @@ export const SmallNavigation = () => {
     <div
       className={cn(
         "fixed inset-x-0 top-0 z-50 flex h-22 flex-col",
-        isOpen && "h-dvh bg-background",
+        isOpen && "h-dvh bg-white",
       )}
     >
       <div className="mx-auto flex h-full w-full max-w-4xl flex-1 flex-col">
-        <header className="relative flex w-full items-center justify-between bg-linear-to-b from-background via-background/90 p-6">
+        <header className="relative flex w-full items-center justify-between bg-linear-to-b from-white via-white/90 p-6">
           <Link href="/aura">
             <AuraLogo className="text-2xl" />
           </Link>
@@ -28,7 +28,7 @@ export const SmallNavigation = () => {
           </IconButton>
         </header>
         {isOpen && (
-          <ScrollArea className="flex-1 bg-background px-3">
+          <ScrollArea className="flex-1 bg-white">
             <div className="px-3">
               <Navigation onSelect={() => setIsOpen(false)} />
             </div>
