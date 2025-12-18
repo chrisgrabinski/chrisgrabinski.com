a8f29e2aefd25a6ffbb8cb4702b027c3a9afe530
Chris Grabiński <c.grabinski@gmail.com>
2025-12-18 17:42:28 +0000

    improve page code and fix bradcrumbs

--- src/app/aura/components/[...component]/page.tsx
+++ src/app/aura/components/[...component]/page.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { ComponentCanvas } from "@/app/aura/component-canvas";
 import { ComponentPreview } from "@/app/aura/component-preview";
-import { component as components } from "@/app/aura/components/components";
+import {
+  type Component,
+  component as componentConfig,
+} from "@/app/aura/components/components";
 import * as patterns from "@/app/aura/patterns";
 import { Breadcrumbs } from "@/components/breadcrumbs";
 import { Button } from "@/components/button";
@@ -10,27 +14,52 @@ import { Card } from "@/components/card";
 import { GitHubIcon } from "@/icons/github";
 import { StorybookIcon } from "@/icons/storybook";
 
-const getComponentData = (componentName: string) => {
-  if (componentName in components) {
-    return components[componentName as keyof typeof components];
-  }
-
-  notFound();
+const getComponentName = (component: string[]) => {
+  return component.join("/");
 };
 
-// export async function generateStaticParams() {
-//   return components.map((component) => ({
-//     component: component.name,
-//   }));
-// }
+const getComponentsData = (component: string[]) => {
+  const componentNames = component.map((name, index) => {
+    return component.slice(0, index + 1).join("/");
+  });
+
+  return componentNames.map((name) => componentConfig[name]);
+};
+
+const getVariantsData = (component: string[]) => {
+  const componentName = getComponentName(component);
+  const expression = new RegExp(`^(?:/)?${componentName}(?:/).+(?:/)?$`);
+
+  return Object.keys(componentConfig).reduce<Component[]>(
+    (accumulator, currentValue) => {
+      console.log(currentValue, expression.test(currentValue));
+      if (expression.test(currentValue)) {
+        accumulator.push({
+          ...componentConfig[currentValue as keyof typeof componentConfig],
+          name: currentValue,
+        });
+      }
+
+      return accumulator;
+    },
+    [],
+  );
+};
+
+export async function generateStaticParams() {
+  return Object.keys(componentConfig).map((component) => ({
+    component: component.split("/"),
+  }));
+}
 
 export async function generateMetadata({
   params,
 }: PageProps<"/aura/components/[...component]">): Promise<Metadata> {
   const { component } = await params;
 
-  const componentName = component.join("/");
-  const componentData = getComponentData(componentName);
+  const componentsData = getComponentsData(component);
+
+  const componentData = componentsData[componentsData.length - 1];
 
   return {
     description: componentData.description,
@@ -43,12 +72,21 @@ export default async function ComponentPage({
 }: PageProps<"/aura/components/[...component]">) {
   const { component } = await params;
 
-  const componentName = component.join("/");
-  const componentData = getComponentData(componentName);
+  const componentName = getComponentName(component);
 
-  const mainDemo = Object.values(patterns).find(
+  if (!(componentName in componentConfig)) {
+    notFound();
+  }
+
+  const componentsData = getComponentsData(component);
+
+  const componentData = componentsData[componentsData.length - 1];
+
+  const componentDemo = Object.values(patterns).find(
     (pattern) => pattern.name === componentName,
   );
+
+  const variantsData = getVariantsData(component);
 
   const componentPatterns = Object.values(patterns)
     .filter((pattern) => pattern.components.includes(componentName))
@@ -62,11 +100,11 @@ export default async function ComponentPage({
           includeJsonLd
           items={[
             { name: "Home", url: "/aura" },
-            {
+            // @ts-expect-error - TODO: Look into dynamic route segments with typed routes
+            ...componentsData.map((componentData) => ({
               name: componentData.title,
-              // @ts-expect-error - TODO: Look into dynamic route segments with typed routes
-              url: `/aura/components/${component}`,
-            },
+              url: `/aura/components/${componentData.name}`,
+            })),
           ]}
         />
         <div className="grid gap-1.5">
@@ -100,31 +138,32 @@ export default async function ComponentPage({
           )}
         </div>
       </div>
-      <ComponentPreview>{mainDemo && <mainDemo.Component />}</ComponentPreview>
-      {/* {componentData.variants && (
+      <ComponentPreview>
+        {componentDemo && <componentDemo.Component />}
+      </ComponentPreview>
+      {variantsData.length > 0 && (
         <div className="grid gap-6">
           <h2 className="font-medium text-2xl">Variants</h2>
           <div className="grid gap-6 sm:grid-cols-2">
-            {componentData.variants.map((variant) => (
+            {variantsData.map((variant) => (
               <Link
                 className="font-medium text-lg"
-                href={`/aura/components/${component}/${variant.name}`}
+                href={`/aura/components/${variant.name}`}
                 key={variant.name}
               >
-                <Card className="flex flex-col justify-end sm:aspect-2/1">
+                <Card className="flex flex-col justify-end">
                   {variant.title}
                 </Card>
               </Link>
             ))}
           </div>
         </div>
-      )} */}
+      )}
       {componentPatterns.length > 0 && (
         <div className="grid gap-6">
           <h2 className="font-medium text-2xl">Examples</h2>
           {componentPatterns.map(({ Component, name }) => (
             <div className="grid gap-3" key={name}>
-              {/* <h3 className="font-medium text-lg">{title}</h3> */}
               <Card className="p-0">
                 <ComponentCanvas>
                   <Component />
