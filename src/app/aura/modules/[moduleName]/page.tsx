3db2d2c903f4b395b5315b33720fe9d3abb18f4f
Chris Grabiński <c.grabinski@gmail.com>
2026-06-07 14:26:31 +0100

    update links

--- src/app/aura/modules/[moduleName]/page.tsx
+++ src/app/aura/modules/[moduleName]/page.tsx
@@ -55,7 +55,7 @@ export default async function TestPage({
         breadcrumbs={[
           {
             name: "Modules",
-            url: "/aura/modules",
+            url: "/aura/modules/",
           },
           {
             name: title,
