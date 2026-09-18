4dd1d529fd1f1787a5522298104ee50b864e9552
Chris Grabiński <c.grabinski@gmail.com>
2026-09-18 23:01:40 +0100

    add header background effect

--- src/app/aura/layout.tsx
+++ src/app/aura/layout.tsx
@@ -15,15 +15,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/aura">) {
   return (
     <div className="flex min-h-dvh flex-col">
-      <header className="sticky top-0 z-10 flex h-18 items-center justify-between bg-linear-180 from-background via-background/60 px-4 md:px-8">
-        <Link
-          className="focus-visible:focus-ring grid h-8 place-items-center rounded-action"
-          href="/aura"
-        >
-          <AuraLogo className="h-7" />
-        </Link>
-        <ThemeSwitcher />
-        <SmallNavigation />
+      <header className="sticky top-0 z-10">
+        <div className="relative z-10 flex h-18 items-center justify-between bg-linear-180 from-background via-background/60 px-4 md:px-8">
+          <Link
+            className="focus-visible:focus-ring grid h-8 place-items-center rounded-action"
+            href="/aura"
+          >
+            <AuraLogo className="h-7" />
+          </Link>
+          <ThemeSwitcher />
+          <SmallNavigation />
+        </div>
+        <div className="mask-b-from-40% mask-b-to-100% absolute inset-0 backdrop-blur-xs backdrop-saturate-200" />
       </header>
       <div className="flex grow justify-between gap-8 px-4 md:px-8">
         <nav className="hidden w-48 shrink-0 md:block">
