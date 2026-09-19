2c3635259d42982feba52ace3efd6cbde5cd916d
Chris Grabiński <c.grabinski@gmail.com>
2026-09-19 14:28:27 +0100

    homepage adjustments

--- src/app/aura/page.tsx
+++ src/app/aura/page.tsx
@@ -6,34 +6,32 @@ import { Text } from "@/components/text";
 
 export default function RootPage() {
   return (
-    <div className="mx-auto grid max-w-7xl gap-8 p-8">
-      <div className="col-span-full h-[50dvh]" />
-      <AuraLogo className="h-12" />
-      <div className="grid gap-[inherit] lg:grid-cols-2">
-        <div>
-          <div className="grid gap-8">
-            <Text as="p" className="max-w-lg text-pretty" muted size={5}>
+    <div className="grid gap-8">
+      <div className="col-span-full h-[50dvh] bg-linear-to-t from-accent via-purple-500 to-blue-900" />
+      <div className="mx-auto grid w-full max-w-7xl gap-8 p-8">
+        <AuraLogo className="h-12" />
+        <div className="flex flex-col gap-inherit md:flex-row">
+          <div>
+            <Text as="p" className="max-w-md text-pretty" muted size={5}>
               A collection of modular components and UI patterns designed for
-              modern web applications. This project serves as a practical design
-              system, focused on refining architectural patterns and testing new
-              development concepts.
+              modern web applications.
             </Text>
           </div>
-        </div>
-        <div className="grid gap-[inherit]">
-          <Overline>Components</Overline>
-          <div className="grid max-w-3xl grid-cols-2 gap-4 md:grid-cols-3">
-            {Object.values(componentsData).map((component) => {
-              return (
-                <Link
-                  // @ts-expect-error - TODO: fix link types
-                  href={`/aura/components/${component.name}`}
-                  key={component.name}
-                >
-                  {component.title}
-                </Link>
-              );
-            })}
+          <div className="grid flex-1 gap-8 pb-8">
+            <Overline>Components</Overline>
+            <div className="grid max-w-3xl grid-cols-2 gap-4 lg:grid-cols-3">
+              {Object.values(componentsData).map((component) => {
+                return (
+                  <Link
+                    // @ts-expect-error - TODO: fix link types
+                    href={`/aura/components/${component.name}`}
+                    key={component.name}
+                  >
+                    {component.title}
+                  </Link>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
