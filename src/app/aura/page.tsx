8ca30eca84f9ba4c975cd0c710a837767e8a5dca
Chris Grabiński <c.grabinski@gmail.com>
2026-09-19 14:45:47 +0100

    even more homepage adjustments

--- src/app/aura/page.tsx
+++ src/app/aura/page.tsx
@@ -1,37 +1,36 @@
+import Link from "next/link";
 import * as componentsData from "@/app/aura/(docs)/data/components";
 import { AuraLogo } from "@/app/aura/aura-logo";
-import { Link } from "@/components/link";
 import { Overline } from "@/components/overline";
 import { Text } from "@/components/text";
 
 export default function RootPage() {
   return (
-    <div className="grid gap-8">
+    <div className="grid">
       <div className="col-span-full h-[50dvh] bg-linear-to-t from-accent via-purple-500 to-blue-900" />
-      <div className="mx-auto grid w-full max-w-7xl gap-8 p-8">
-        <AuraLogo className="h-12" />
-        <div className="flex flex-col gap-inherit md:flex-row">
-          <div>
-            <Text as="p" className="max-w-md text-pretty" muted size={5}>
-              A collection of modular components and UI patterns designed for
-              modern web applications.
-            </Text>
-          </div>
-          <div className="grid flex-1 gap-8 pb-8">
-            <Overline>Components</Overline>
-            <div className="grid max-w-3xl grid-cols-2 gap-4 lg:grid-cols-3">
-              {Object.values(componentsData).map((component) => {
-                return (
-                  <Link
-                    // @ts-expect-error - TODO: fix link types
-                    href={`/aura/components/${component.name}`}
-                    key={component.name}
-                  >
-                    {component.title}
-                  </Link>
-                );
-              })}
-            </div>
+      <div className="mx-auto flex w-full max-w-7xl flex-col items-start gap-8 p-8 lg:flex-row">
+        <div className="grid shrink-0 gap-4">
+          <AuraLogo className="h-10" />
+          <Text as="p" className="max-w-md text-pretty" muted size={5}>
+            A collection of modular components and UI patterns designed for
+            modern web applications.
+          </Text>
+        </div>
+        <div className="flex w-full grow flex-col gap-4">
+          <div className="hidden h-10 lg:block" />
+          <Overline>Components</Overline>
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3">
+            {Object.values(componentsData).map((component) => {
+              return (
+                <Link
+                  className="border-2 p-4 font-mono hover:text-accent"
+                  href={`/aura/components/${component.name}`}
+                  key={component.name}
+                >
+                  {component.title}
+                </Link>
+              );
+            })}
           </div>
         </div>
       </div>
